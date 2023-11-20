98feb2d0c3953902b80523ad2ef4a0a71fd96bbd
腾飞工作室 <867805932@qq.com>
2023-11-21 01:48:31 +0800

    支持多行sql转md table,注意sql用;分割

--- src/sql2md.js
+++ src/sql2md.js
@@ -1,35 +1,44 @@
 'use strict';
+const _ = require('lodash');
 
 /**
- * sql 转 markdown 表格
+ * insert sql 转 markdown 表格
  */
 function insertSqlToMarkdown(sql) {
+    // 使用lodash库的split方法将sql语句按照";"分割成数组
+    const sqlList = _.split(sql, ';');
+
+    const firstSql=sqlList[0];
     // 提取表名和列名
     // const tableName = sql.match(/INTO\s+([^\s]+)/i)[1];
-    const columns = sql.match(/\(([^)]+)\)/is)[1].split(',');
-
-    // 提取值
-    const valuesMatch = sql.match(/VALUES\s+\(([^)]+)\)/is);
-    const values = valuesMatch ? valuesMatch[1].split(',') : [];
+    const columns = firstSql.match(/\(([^)]+)\)/is)[1].split(',');
 
     // 构建表头
     let markdownTable = '| ';
     markdownTable += columns.join(' | ');
     markdownTable += ' |\n';
 
-    // 构建分隔线
-    let separator = '|';
-    for (let i = 0; i < columns.length; i++) {
-        separator += '---------|';
-    }
-    markdownTable += separator + '\n';
+     // 构建分隔线
+     let separator = '|';
+     for (let i = 0; i < columns.length; i++) {
+         separator += '---------|';
+     }
+     markdownTable += separator + '\n';
 
-    // 构建值行
-    if (values.length > 0) {
-        markdownTable += '| ';
-        markdownTable += values.map(value => value.trim()).join(' | ');
-        markdownTable += ' |\n';
-    }
+    
+     // 构建值行
+    sqlList.forEach(statement => {
+        // 提取值
+        const valuesMatch = statement.match(/VALUES\s+\(([^)]+)\)/is);
+        const values = valuesMatch ? valuesMatch[1].split(',') : [];
+
+        // 每个值一行
+        if (values.length > 0) {
+            markdownTable += '| ';
+            markdownTable += values.map(value => value.trim()).join(' | ');
+            markdownTable += ' |\n';
+        }
+    });
 
     return markdownTable;
 }
